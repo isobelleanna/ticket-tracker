9991177297b8d31d6cb3f04e12e0cbfeb35e9a8e
isobelleanna <67749649+isobelleanna@users.noreply.github.com>
2022-06-08 14:18:51 +0100

    adjust styling

--- src/components/CounterCard/CounterCard.jsx
+++ src/components/CounterCard/CounterCard.jsx
@@ -11,17 +11,17 @@ const CounterCard = () => {
    const handleDecrease = () => {
      counter === 0 ? setCounter(0): setCounter(counter - 1)
   }
-  
+
   return (
     <div className='counter-card'>
         <h4 className='counter-card__title'>Counter</h4>
         <p className='counter-card__number'>{counter}</p>
         <div className='counter-card__button-bar'>
+          <div onClick={handleDecrease}>
+        <Button buttonText="-" />
+        </div>
         <div onClick={handleIncrease}>
         <Button buttonText="+" />
-        </div>
-        <div onClick={handleDecrease}>
-        <Button buttonText="-" />
         </div>
         </div>
     </div>
